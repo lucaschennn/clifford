dc07d98212e8d130c3637f33c184dd22dc43ff95
lucaschennn <lucasch@umich.edu>
2023-03-22 00:43:20 -0400

    removed mui (from last commit) and worked on styles and layout for dashboard.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,32 +1,31 @@
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Dashboard() {
-    return (
-        <div className="row">
-            <div className='col-2' id='dashboard-sidebar'>
 
-                <Nav defaultActiveKey="/home" className="flex-column">
-                    <h5 className='m-2'>Dashboard</h5>
-                    <div id="dashboard-profile-icon">
-                        <img id="dashboard-profile-icon-img" src="/images/thumbnails/sample_1.jpg"/>
-                    </div>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                    <Nav.Link eventKey="link-1">Link</Nav.Link>
-                    <Nav.Link eventKey="link-2">Link</Nav.Link>
-                </Nav>
-            </div>
-            <div className='col-4'>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-            </div>
+    const { user, isAuthenticated} = useAuth0();
+    const date = new Date();
 
+    return ( isAuthenticated &&
+        <div className="row w-100">
+            <div id='dashboard-sidebar'>
+                <Navbar>
+                    <Nav defaultActiveKey="/" className="flex-column">
+                        <Nav.Link href="/" className="clifford-text">Clifford</Nav.Link>
+                        <div id="dashboard-profile-icon"></div>
+                        <Nav.Link href="/home">Dashboard</Nav.Link>
+                        <Nav.Link href="/home">Products</Nav.Link>
+                        <Nav.Link eventKey="link-1">Reviews</Nav.Link>
+                        <Nav.Link eventKey="link-2">Analytics</Nav.Link>
+                        <Nav.Link eventKey="link-2">Settings</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </div>
+            <div className='col-8' id='dashboard-content'>
+                <h4 className="text-clifford-pink"> Welcome back, {user.name}!</h4>
+                <h5 className="text-muted" id="date-string">{date.toDateString()}</h5>
+            </div>
         </div>
     )
 }
